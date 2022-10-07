57af1ad751bd4da9324fe1a6562ffcec38158b8d
Briar <briar.j.mills@gmail.com>
2022-10-07 15:12:38 -0400

    Add test_add_chirps_to_dataset

--- tests/test_chirps.py
+++ tests/test_chirps.py
@@ -2,6 +2,7 @@ import pytest
 
 from os.path import join
 from hdx.api.configuration import Configuration
+from hdx.data.dataset import Dataset
 from hdx.utilities.path import temp_dir
 from hdx.utilities.useragent import UserAgent
 from chirps import *
@@ -16,7 +17,7 @@ class TestChirps:
         Configuration._create(
             hdx_read_only=True,
             hdx_site="prod",
-            project_config_yaml=join("tests", "config", "project_configuration.yml"),
+            project_config_yaml=join("config", "project_configuration.yml"),
         )
         return Configuration.read()
 
@@ -46,3 +47,23 @@ class TestChirps:
     def test_get_latest_data(self, configuration, downloader):
         latest_url = get_latest_data(configuration["base_url"], downloader)
         assert latest_url == configuration["base_url"]+TestChirps.latest_url
+
+    def test_add_chirps_to_dataset(self, configuration, downloader):
+        dataset = Dataset.load_from_json(join(
+            "tests", "fixtures", "east-africa-chirps-seasonal-rainfall-accumulation-anomaly-by-pentad.json"
+        ))
+        dataset, updated = add_chirps_to_dataset(
+            dataset,
+            TestChirps.latest_url,
+            "March to May 2022 (Mar pentad 1 thru May Pentad 6) - Average(1981-2010)\nPentad: ")
+        dataset.get_date_of_dataset()
+        assert dataset["dataset_date"] == "[2022-05-25T00:00:00 TO 2022-05-31T00:00:00]"
+        assert updated is True
+        assert dataset.get_resources()[0] == {
+            "name": "ea_chirps_seasaccum_anom_marmay_202230_lta",
+            "description": "March to May 2022 (Mar pentad 1 thru May Pentad 6) - Average(1981-2010)\nPentad: 30",
+            "url": "ea_chirps_seasaccum_anom_marmay_202230_lta.zip",
+            "format": "geotiff",
+            "resource_type": "api",
+            "url_type": "api"
+        }
